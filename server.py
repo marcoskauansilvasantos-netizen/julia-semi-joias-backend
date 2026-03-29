88ea36cabd050e97374d717f17165edc73da4ce7
marcoskauansilvasantos-netizen <marcoskauansilvasantos@gmail.com>
2026-03-29 17:48:46 -0300

    Update server.py

--- server.py
+++ server.py
@@ -24,7 +24,15 @@ db = client[os.environ['DB_NAME']]
 
 # Create the main app without a prefix
 app = FastAPI()
+from fastapi.middleware.cors import CORSMiddleware
 
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["*"],
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
 # Create a router with the /api prefix
 api_router = APIRouter(prefix="/api")
 
